051ff317bd4ab94af60685e24b58e6e2e7fbb07c
HYUNJo3o <sodapop47@cau.ac.kr>
2020-07-17 04:54:36 +0900

    crud - detail error

--- crud_app/views.py
+++ crud_app/views.py
@@ -1,4 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Blog
+from django.utils import timezone
 
 def home(request):
-    return render(request, 'home.html')
+    blogs = Blog.objects
+    return render(request, 'home.html', { 'blogs': blogs })
+
+def detail(request, blog_id):
+    details = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'detail.html')
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    blog = Blog()
+    blog.title = request.GET['title'] #new.html의 name="title"
+    blog.body = request.GET['body'] #new.html의 name="body"
+    blog.pub_date = timezone.datetime.now()
+    blog.save()
+    return redirect('home')
+
+# C
+
+def edit(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'edit.html', { 'blog': blog })
+
+def update(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    blog.title = request.GET['title'] #new.html의 name="title"
+    blog.body = request.GET['body'] #new.html의 name="body"
+    blog.pub_date = timezone.datetime.now()
+    blog.save()
+    return redirect('home')
+
+# R
+
+def delete(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    blog.delete()
+    return redirect('home')
+
+# D
--- crud_project/urls.py
+++ crud_project/urls.py
@@ -1,10 +1,17 @@
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 import crud_app.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', crud_app. views. home, name="home")
+    path('', crud_app. views. home, name="home"),
+    path('blog/<int:blog_id>', crud_app.views.detail, name="detail"),
+    path('new/', crud_app.views.new, name="new"),
+    path('blog/create', crud_app.views.create, name="create")
 ]
 
-# 으아아ㅏㅏ,,, url 연결하는 거 이해 안 됨 ,,,,,,,,,,,,, ㅇ<-<
+
+urlpatterns += staticfiles_urlpatterns()
